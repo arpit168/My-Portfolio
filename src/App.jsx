fb1591cafe17e79715867156f71057a19254fe40
Arpit Gupta <arpitgupta0406@gmail.com>
2026-03-12 02:24:08 +0530

    portfolio basic has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import ParticlesBackground from "./components/ParticlesBackground";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
 
-const App = () => {
+
+
+export default function App(){
+
   return (
-    <div>
-      App
+    <div className="relative gradient text-white">
+      <ParticlesBackground/>
+
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+      
     </div>
   )
 }
-
-export default App
---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomCursor = () => {
+  return (
+    <div>
+      custom cursor
+    </div>
+  )
+}
+
+export default CustomCursor
