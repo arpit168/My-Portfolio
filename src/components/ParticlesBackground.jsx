1d08cb455d84f779a6cd29dc9a9621ca65c898b0
Arpit Gupta <arpitgupta0406@gmail.com>
2026-03-12 17:57:57 +0530

    home page is in progress

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 
-export default function ParticlesBackgroun() {
+export default function ParticlesBackground() {
     const CanvasRef = useRef(null);
 
     useEffect(() => {
