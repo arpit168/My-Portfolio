1d2026c462157c9b276e0d41a9840d1332b36c95
Arpit Gupta <arpitgupta0406@gmail.com>
2026-03-15 12:18:30 +0530

    overlay menu has been created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300  ${
           visible ? "translate-y-0" : "-translate-y-full"
         }`}
       >
